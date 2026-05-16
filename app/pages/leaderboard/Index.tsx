92a5dd787598b324ae8a637adafa3f5ec2a51304
Wynn-DEX <jreacher9@proton.me>
2026-05-17 00:33:21 +0700

    Update Index.tsx

--- app/pages/leaderboard/Index.tsx
+++ app/pages/leaderboard/Index.tsx
@@ -7,7 +7,7 @@ export default function LeaderboardIndex() {
   const pageMeta = getPageMeta();
   const pageTitle = generatePageTitle("Leaderboard");
 
-  return (
+  return ( 
     <>
       {renderSEOTags(pageMeta, pageTitle)}
       <div className="oui-py-6 oui-px-4 lg:oui-px-6 xl:oui-pl-4 lx:oui-pr-6">
