e1565cba927e505280bc0216a8bbe0b84ebeddee
Wynn-DEX <jreacher9@proton.me>
2026-05-17 08:03:32 +0700

    Update Symbol.tsx

--- app/pages/perp/Symbol.tsx
+++ app/pages/perp/Symbol.tsx
@@ -41,10 +41,11 @@ export default function PerpSymbol() {
         onSymbolChange={onSymbolChange}
         tradingViewConfig={config.tradingPage.tradingViewConfig}
         sharePnLConfig={config.tradingPage.sharePnLConfig}
+      />
       <div className="md:hidden pb-2 pt-8 text-center">
         <span className="oui-text-2xs oui-text-base-contrast-54">
           Charts powered by{" "}
-          
+          <a
             href="https://tradingview.com"
             target="_blank"
             rel="noopener noreferrer"
