0b802e9bb9eeabbca4fb0a1617d37d53d2f5e87b
Wynn-DEX <jreacher9@proton.me>
2026-05-18 18:53:34 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Portfolio,Leaderboard",
-  "VITE_CUSTOM_MENUS": "About WYNN DEX,/about",
+  "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/Wynn_Trades",
